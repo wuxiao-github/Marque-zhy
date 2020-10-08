bb3a4623e5a9b32e98fe0cf50614507e876d6cd5
wuxiao <wuxiao0063@163.com>
2020-10-08 13:05:46 +0800

    zhy  走马灯和聊天室

---
+++ zhy/app/src/main/java/com/example/zhy/ClickActivity.java
@@ -0,0 +1,19 @@
+package com.example.zhy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ClickActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_click);
+
+
+
+
+
+    }
+}
---
+++ zhy/app/src/main/java/com/example/zhy/util/DateUtil.java
@@ -0,0 +1,28 @@
+package com.example.zhy.util;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateUtil {
+
+
+    public static String getNowDateTime() {
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmss");
+
+        return sdf.format(new Date());
+
+    }
+    public static String getNowTime() {
+
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+
+        return sdf.format(new Date());
+
+    }
+
+
+
+
+
+}
